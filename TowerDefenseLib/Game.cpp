576d9bd219ced7b616437645182136f4f517498b
jacksonwhite244 <jacksonwhite244@gmail.com>
2025-05-11 18:35:38 -0400

    Tower Defense Project with working TowerDefenseLib

--- TowerDefenseLib/Game.cpp
+++ TowerDefenseLib/Game.cpp
@@ -1,6 +1,33 @@
 /**
  * @file Game.cpp
- * @author jacks
+ * @author Jackson White
  */
  
 #include "Game.h"
+
+Game::Game()
+{
+
+}
+
+Game::~Game()
+{
+
+}
+
+/**
+ * Update the game
+ */
+void Game::Update()
+{
+
+}
+
+/**
+ * visualize the game
+ */
+void Game::Render()
+{
+
+}
+
--- TowerDefenseLib/Game.h
+++ TowerDefenseLib/Game.h
@@ -1,19 +1,36 @@
 /**
  * @file Game.h
- * @author jacks
+ * @author Jackson White
  *
- *
+ * Game class that runs the game
  */
  
 #ifndef GAME_H
 #define GAME_H
 
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
 
-
+/**
+ * Game class that runs / controls the game
+ */
 class Game {
 private:
+    void MakeWindow();
 
 public:
+    /// constructor
+    Game();
+
+    /// virtual destructor
+    virtual ~Game();
+
+    void Update();
+
+    void Render();
 
 };
 
